be7bb8ffe30e026a5e2d1275de4559490f75f665
Tamu <5020311+Tamu@users.noreply.github.com>
2026-04-29 15:21:11 +0200

    Add favicons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,4 +6,16 @@ export default defineNuxtConfig({
     '@pinia/nuxt',
   ],
   css: ['@/assets/css/main.css'],
+  app: {
+    head: {
+      title: 'TenantReport',
+      link: [
+        { 
+          rel: 'icon', 
+          type: 'image/svg+xml', 
+          href: 'data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="%233b82f6" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M22 12h-4l-3 9L9 3l-3 9H2"/></svg>' 
+        }
+      ]
+    }
+  }
 })
